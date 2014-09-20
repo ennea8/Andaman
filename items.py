ffd7a623b275f3d83e01a63b71ee9a235a6c20d7
wdx <996931657@qq.com>
2014-09-20 16:16:36 +0800

    修改item和run脚本

--- items.py
+++ items.py
@@ -6,15 +6,130 @@
 # http://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.item import Item, Field
 
 
-class WeatherItem(scrapy.Item):
-    # define the fields for your item here like:
-    id = scrapy.Field()
-    province = scrapy.Field()
-    city = scrapy.Field()
-    county = scrapy.Field()
+class SightItem(Item):
+    web_name = Field()
+    sight_url = Field()
+    name = Field()
+    province = Field()
+    city = Field()
+    address = Field()
+    theme = Field()
+    price = Field()
+    phone = Field()
+    opentime = Field()
+    rate = Field()
+    intro = Field()
+    image_urls = Field()
+    images = Field()
+
+
+class YiqiqusightItem(SightItem):
+    web_name = Field(SightItem.fields['web_name'])
+    sight_url = Field(SightItem.fields['sight_url'])
+    name = Field(SightItem.fields['name'])
+    province = Field(SightItem.fields['province'])
+    city = Field(SightItem.fields['city'])
+    address = Field(SightItem.fields['address'])
+    theme = Field(SightItem.fields['theme'])
+    price = Field(SightItem.fields['price'])
+    phone = Field(SightItem.fields['phone'])
+    opentime = Field(SightItem.fields['opentime'])
+    rate = Field(SightItem.fields['rate'])
+    intro = Field(SightItem.fields['intro'])
+    image_urls = Field(SightItem.fields['image_urls'])
+    images = Field(SightItem.fields['images'])
+    reasons = Field()
+    notice = Field()
+    traffic = Field()
+    food = Field()
+    desc = Field()
+    spots = Field()
+    culture = Field()
+
+
+class BlogItem(Item):
+    web_name = Field()
+    author_url = Field()
+    author_id = Field()
+    author_name = Field()
+    blog_url = Field()
+    date = Field()
+    title = Field()
+    tag = Field()
+    hot = Field()
+    image_urls = Field()
+    images = Field()
+
+
+class MafengwoblogItem(BlogItem):
+    web_name = Field(BlogItem.fields['web_name'])
+    author_id = Field(BlogItem.fields['author_id'])
+    author_name = Field(BlogItem.fields['author_name'])
+    author_url = Field(BlogItem.fields['author_url'])
+    blog_url = Field(BlogItem.fields['blog_url'])
+    date = Field(BlogItem.fields['date'])
+    title = Field(BlogItem.fields['title'])
+    tag = Field(BlogItem.fields['tag'])
+    hot = Field(BlogItem.fields['hot'])
+    image_urls = Field(BlogItem.fields['image_urls'])
+    images = Field(BlogItem.fields['images'])
+    desc = Field()
+    is_recommend = Field()
+    play_time = Field()
+    type = Field()
+    days = Field()
+    cost = Field()
+    person = Field()
+
+
+class ZailushangItem(BlogItem):
+    web_name = Field(BlogItem.fields['web_name'])
+    author_url = Field(BlogItem.fields['author_url'])
+    author_name = Field(BlogItem.fields['author_name'])
+    author_id = Field(BlogItem.fields['author_id'])
+    blog_url = Field(BlogItem.fields['blog_url'])
+    title = Field(BlogItem.fields['title'])
+    date = Field(BlogItem.fields['date'])
+    tag = Field(BlogItem.fields['tag'])
+    like_num = Field(BlogItem.fields['hot'])
+    image_urls = Field(BlogItem.fields['image_urls'])
+    images = Field(BlogItem.fields['images'])
+    cmt_num = Field()
+    preface = Field()
+    sights = Field()
+    content = Field()
+
+
+class BreadtripItem(BlogItem):
+    web_name = Field(BlogItem.fields['web_name'])
+    author_url = Field(BlogItem.fields['author_url'])
+    blog_url = Field(BlogItem.fields['blog_url'])
+    title = Field(BlogItem.fields['title'])
+    date = Field(BlogItem.fields['date'])
+    like_num = Field(BlogItem.fields['hot'])
+    image_urls = Field(BlogItem.fields['image_urls'])
+    images = Field(BlogItem.fields['images'])
+    cmt_num = Field()
+    share_num = Field()
+    sights = Field()
+    content = Field()
+    days = Field()
+
+
+class JsonItem(scrapy.Item):
     data = scrapy.Field()
+
+
+class BaiduPoiItem(JsonItem):
+    pass
+
+
+class QunarPoiItem(JsonItem):
+    pass
+
 
 class QiniuyunItem(scrapy.Item):
     # define the fields for your item here like:
@@ -23,3 +138,81 @@ class QiniuyunItem(scrapy.Item):
     url = scrapy.Field()
     key = scrapy.Field()
     hash_value = scrapy.Field()
+
+
+class TravelNotesItem(scrapy.Item):
+    user_name = scrapy.Field()
+    user_url = scrapy.Field()
+
+    note_url = scrapy.Field()
+    note_list = scrapy.Field()
+
+    start_year = scrapy.Field()
+    start_month = scrapy.Field()
+    origin = scrapy.Field()
+    destination = scrapy.Field()
+    time = scrapy.Field()
+    cost = scrapy.Field()
+
+    quality = scrapy.Field()
+
+    title = scrapy.Field()
+    reply = scrapy.Field()
+    view = scrapy.Field()
+    recommend = scrapy.Field()
+    favourite = scrapy.Field()
+
+    sub_note = scrapy.Field()
+
+
+class ChanyoujiUser(scrapy.Item):
+    user_id = scrapy.Field()
+    user_name = scrapy.Field()
+    num_notes = scrapy.Field()
+    weibo_url = scrapy.Field()
+    weibo_uid = scrapy.Field()
+    douban_url = scrapy.Field()
+    douban_uid = scrapy.Field()
+    renren_url = scrapy.Field()
+    renren_uid = scrapy.Field()
+    traveled = scrapy.Field()
+
+
+class ChanyoujiYoujiItem(scrapy.Item):
+    trips_id = scrapy.Field()
+    data = scrapy.Field()
+
+
+class MafengwoYoujiItem(scrapy.Item):
+    place = scrapy.Field()  #景观地点
+    author = scrapy.Field() #游记作者
+    title = scrapy.Field()  #游记标题
+    public_time = scrapy.Field()#游记发表时间
+    cost = scrapy.Field()   #人均费用
+    way = scrapy.Field()    #旅行方法
+    days = scrapy.Field()   #旅行用的时间
+    contents = scrapy.Field()#内容
+    reply = scrapy.Field()  #回复
+
+
+class WeatherItem(scrapy.Item):
+    data = scrapy.Field()
+    loc = scrapy.Field()
+
+class BreadTripItem(scrapy.Item):
+    city_name = scrapy.Field()
+    city_id = scrapy.Field()
+    trip_info = scrapy.Field()
+    blog = scrapy.Field()
+    trip_days = scrapy.Field()
+    user_url = scrapy.Field()
+    user_img = scrapy.Field()
+
+class BaiduTripItem(scrapy.Item):
+    scene_info = scrapy.Field()
+
+class YikuaiquItem(scrapy.Item):
+    spot_info = scrapy.Field()
+
+class YikuaiquSpotItem(scrapy.Item):
+    spot_info = scrapy.Field()
--- scripts/run_crawler.py
+++ scripts/run_crawler.py
@@ -1,11 +1,18 @@
 # coding=utf-8
+import sys
+
+import scrapy
+from spiders.MafengwoSpider import MafengwoYoujiSpider
+
 from scrapy import signals
 from scrapy.crawler import Crawler
 from scrapy.settings import Settings
 from twisted.internet import reactor
-from spiders.PicSpider import PicSpider
-# from spiders.weather_spider import WeatherSpider
 
+# from spiders.weather_spider import WeatherSpider
+from spiders.notes.baidu_notes import BaiduNoteSpider
+from spiders.MafengwoSpider import MafengwoYoujiSpider
+from spiders.ChanyoujiYoujiSpider import ChanyoujiYoujiSpider
 
 __author__ = 'zephyre'
 
@@ -13,20 +20,59 @@ __author__ = 'zephyre'
 def setup_spider():
     crawler = Crawler(Settings())
     crawler.signals.connect(reactor.stop, signal=signals.spider_closed)
-    # crawler.settings.set('ITEM_PIPELINES', {'pipelines.TravelcrawlerPipeline': 800})
-    crawler.settings.set('ITEM_PIPELINES', {'pipelines.QiniuyunPipeline': 800})
-    crawler.configure()
 
+    settings = crawler.settings
+
+    # settings.setdict({'ITEM_PIPELINES': {'spiders.notes.baidu_notes.BaiduNotePipeline': 200,'pipelines.ChanyoujiYoujiPipline':100}})
+    # settings.setdict({'ITEM_PIPELINES': {'pipelines.ChanyoujiYoujiPipline':800}})
+    settings.set('DOWNLOADER_MIDDLEWARES', {'middlewares.ProxySwitchMiddleware': 300})
+
+    # crawler.settings.set('ITEM_PIPELINES', {'pipelines.BreadtripPipeline': 300})
+    # crawler.settings.set('ITEM_PIPELINES', {'pipelines.ZailushangPipeline': 400})
+
+    crawler.settings.set('ITEM_PIPELINES', {'pipelines.ChanyoujiYoujiPipline': 300})
+    # crawler.settings.set('ITEM_PIPELINES', {'pipelines.MofengwoPipeline': 100})
+    # crawler.settings.set('ITEM_PIPELINES', {'pipelines.YiqiquPipeline': 200})
+
+    # crawler.settings.set('ITEM_PIPELINES', {'scrapy.contrib.pipeline.images.ImagesPipeline': 500})
+
+    settings.set('IMAGES_MIN_HEIGHT', 110)
+    settings.set('IMAGES_MIN_WIDTH', 110)
+
+    crawler.configure()
+    # if argv=='Travel':
     # spider = WeatherSpider()
-    spider = PicSpider()
+    '''if argv == 'mafengwo':
+        spider = MafengwoSpider()
+    elif argv == 'yiqiqu':
+        spider = YiqiquSpider()
+    elif argv == 'zailushang':
+        spider = ZailushangSpider()
+    elif argv == 'breaktrip':
+        spider = BreadtripSpider()'''
+    # spider=MafengwoSpider()
+    # spider=ZailushangSpider()
+    # spider=BreadtripSpider()
+    # spider = YiqiquSpider()
+    # spider = BaiduPoiSpider()
+
+    # spider = QunarPoiSpider(2)
+    # spider = QunarImageSpider()
+    spider = ChanyoujiYoujiSpider()
+
     crawler.crawl(spider)
     crawler.start()
-
     return spider
 
 
 def main():
+    #if len(sys.argv) == 3:
+    #    start = int(sys.argv[1])
+    #    count = int(sys.argv[2])
+    #else:
+    #    start, count = 0, 0
     setup_spider()
+    scrapy.log.start(loglevel=scrapy.log.INFO)
     reactor.run()  # the script will block here until the spider_closed signal was sent
 
 
