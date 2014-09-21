32e3bc5f740974b32d23f93dc8a3c7bc13bcf30d
Zephyre <haizi.zh@gmail.com>
2014-09-21 19:03:25 +0800

    MOD: reformat

--- spiders/ChanyoujiYoujiSpider.py
+++ spiders/ChanyoujiYoujiSpider.py
@@ -1,12 +1,12 @@
-import pymongo
-
 __author__ = 'wdx'
 
-from scrapy import Request ,Selector
-from items import ChanyoujiYoujiItem, ChanyoujiUser
-from scrapy.contrib.spiders import CrawlSpider
 import json
 import re
+
+from scrapy import Request, Selector
+from scrapy.contrib.spiders import CrawlSpider
+
+from items import ChanyoujiYoujiItem
 
 
 class ChanyoujiYoujiSpider(CrawlSpider):
@@ -14,33 +14,32 @@ class ChanyoujiYoujiSpider(CrawlSpider):
 
     def start_requests(self):
         template_url = "http://chanyouji.com/trips/%d"
-        for trips_id in range(1,400000):
+        for trips_id in range(1, 400000):
             url = template_url % trips_id
-            m={'trips_id':trips_id,'url':url}
-            yield Request(url=url,callback=self.parse,meta={"data":m})
+            m = {'trips_id': trips_id, 'url': url}
+            yield Request(url=url, callback=self.parse, meta={"data": m})
 
-    def parse(self,response):
+    def parse(self, response):
 
-        items=[]
-        sel=Selector(response)
+        items = []
+        sel = Selector(response)
         item = ChanyoujiYoujiItem()
         trips_data = response.meta['data']
-        url=trips_data['url']
+        url = trips_data['url']
         item['trips_id'] = trips_data['trips_id']
-        path=sel.xpath('//div/script/text()').extract()
-        text=re.compile("TripsCollection[\s\S]*}]")
+        path = sel.xpath('//div/script/text()').extract()
+        text = re.compile("TripsCollection[\s\S]*}]")
         for content in path:
-            m1=text.search(content)
+            m1 = text.search(content)
             if m1:
-                contents=m1.group()
-                data=contents[16:]
+                contents = m1.group()
+                data = contents[16:]
                 try:
                     data = json.loads(data)
-                    item['data']=data
+                    item['data'] = data
 
                 except ValueError:
                     return
-
 
         items.append(item)
         return items
