b9229dd7c0f6a2adc67a004dc70cfda68f585466
Zephyre <haizi.zh@gmail.com>
2014-09-24 21:25:59 +0800

    MOD: 穷游境外景点修改

--- spiders/QyerAlienspotSpider.py
+++ spiders/QyerAlienspotSpider.py
@@ -1,22 +1,30 @@
-#coding: utf-8
+# coding: utf-8
 __author__ = 'zwh'
-from scrapy import Request, Selector
 import copy
-from scrapy.contrib.spiders import CrawlSpider
-from scrapy.selector import HtmlXPathSelector
-from items import QyerAlienpoiItem
-import pymongo
 import re
-import math
 import json
+
+from scrapy import Request, Selector
+from scrapy.contrib.spiders import CrawlSpider
+import pymongo
+
+from items import QyerAlienpoiItem
+
+
 class QyerAlienSpotSpider(CrawlSpider):
-    name = 'QyerAlienSpotSpider'
+    name = 'qyer_alien'
 
     def __init__(self, *a, **kw):
         super(QyerAlienSpotSpider, self).__init__(*a, **kw)
+        self.param = {}
 
     def start_requests(self):
-        continent_list = ["asia", "europe", "africa", "north-america", "south-america", "oceania"]
+        if 'param' in dir(self):
+            self.param = getattr(self, 'param')
+
+        continent_list = ["asia", "europe", "africa", "north-america", "south-america",
+                          "oceania"] if 'region' not in self.param else [self.param['region'][0].lower()]
+
         for continent in continent_list:
             url = 'http://place.qyer.com/%s' % continent
             yield Request(url=url, callback=self.parse_homepage)
@@ -31,12 +39,15 @@ class QyerAlienSpotSpider(CrawlSpider):
                 if country_tmp:
                     country_url = country_tmp[0]
                 country = item.xpath('./a/text()').extract()
+
+                hot = False
                 if country:
                     country_name = country[0]
                     hot = False
+
                 eng_name = item.xpath('./a/span/text()').extract()
-                if eng_name:
-                    country_engname = eng_name[0]
+                country_engname = eng_name[0].lower() if eng_name else None
+
                 country_tmp = item.xpath('./p[@class="hot"]/a/@href').extract()
                 if country_tmp:
                     country_url = country_tmp[0]
@@ -47,9 +58,18 @@ class QyerAlienSpotSpider(CrawlSpider):
                 eng_name = item.xpath('./p[@class="hot"]/a/span/text()').extract()
                 if eng_name:
                     country_engname = eng_name[0]
+
+                # if 'country' in self.param:
+                # country_param = self.param['country'][0].lower()
+                # if not country_engname or country_engname != country_param:
+                # continue
+
+                if not country_name:
+                    continue
+
                 sights_url = country_url + 'sight'
                 m = {"country_name": country_name, "country_url": country_url, "country_popular": hot,
-                     "country_engname": country_engname,"sights_url": sights_url}
+                     "country_engname": country_engname, "sights_url": sights_url}
                 yield Request(url=sights_url, callback=self.parse_countrysights, meta={"country": m})
 
     def parse_countrysights(self, response):
@@ -67,38 +87,42 @@ class QyerAlienSpotSpider(CrawlSpider):
             if tmp:
                 m["poi_cover"] = tmp[0]
             else:
-                m["poi_cover"] =None
+                m["poi_cover"] = None
             tmp = sight.xpath('./h3/a/text()').extract()
             if tmp:
                 m["poi_name"] = tmp[0]
             tmp = sight.xpath('./div/p[@class="score"]/text()').extract()
             if tmp:
-                m["poi_score"] = tmp[0]
+                tmp = tmp[0]
+                match = re.search('^\s*\d+(\.\d+)?', tmp)
+                if match:
+                    m["poi_score"] = float(match.group())
             else:
                 m["poi_score"] = None
             yield Request(url=m["poi_url"], callback=self.parse_poi, meta={"poi_info": m})
-        tmp = sel.xpath('//div[@id="place_memu_fix"]/div/div[@class="pla_topbtns"]/a[@class="ui_button yelp"]/@onclick').extract()
+        tmp = sel.xpath(
+            '//div[@id="place_memu_fix"]/div/div[@class="pla_topbtns"]/a[@class="ui_button yelp"]/@onclick').extract()
         if tmp:
             # cid = re.search(r'[0-9]+',tmp[0]).group()
-            country_id = int(re.search(r'[0-9]+',tmp[0]).group())
+            country_id = int(re.search(r'[0-9]+', tmp[0]).group())
         tmp = sel.xpath('//div/ul[@id="tab"]/li/a[@data-id="allpoiContent"]/text()').extract()
         if tmp:
-            num = int(re.search(r'[0-9]+',tmp[0]).group())
-            pagenum = int(num/16)
-            if pagenum>1:
+            num = int(re.search(r'[0-9]+', tmp[0]).group())
+            pagenum = int(num / 16)
+            if pagenum > 1:
                 for page in range(pagenum):
                     country_info = copy.deepcopy(country)
-                    body = 'action=ajaxpoi&page=%d&pagesize=16&id=%d&typename=country&cateid=32&orderby=0&tagid=0'%(page+2,country_id)
-                    yield Request(url="http://place.qyer.com/ajax.php", method='POST',body = body,
+                    body = 'action=ajaxpoi&page=%d&pagesize=16&id=%d&typename=country&cateid=32&orderby=0&tagid=0' % (
+                        page + 2, country_id)
+                    yield Request(url="http://place.qyer.com/ajax.php", method='POST', body=body,
                                   headers={'Content-Type': 'application/x-www-form-urlencoded',
                                            'X-Requested-With': 'XMLHttpRequest'},
-                                  callback=self.parse_list, meta={"country": country_info},dont_filter=True)
+                                  callback=self.parse_list, meta={"country": country_info}, dont_filter=True)
 
-    def parse_list(self,response):
+    def parse_list(self, response):
         country = response.meta["country"]
         data = json.loads(response.body)
-        sel = HtmlXPathSelector(text=data['data']['html'])
-        # sel = HtmlXPathSelector(text=response.body)
+        sel = Selector(text=data['data']['html'])
         sights = sel.xpath('//ul/li')
         for sight in sights:
             country_info = copy.deepcopy(country)
@@ -114,7 +138,7 @@ class QyerAlienSpotSpider(CrawlSpider):
             if tmp:
                 m["poi_cover"] = tmp[0]
             else:
-                m["poi_cover"] =None
+                m["poi_cover"] = None
             tmp = sight.xpath('./h3/a/text()').extract()
             if tmp:
                 m["poi_name"] = tmp[0]
@@ -155,7 +179,7 @@ class QyerAlienSpotSpider(CrawlSpider):
                     content = None
             detail.append({"title": tit, "content": content})
         poi_info["poi_detail"] = detail
-        poi_url=poi_info["poi_url"]
+        poi_url = poi_info["poi_url"]
         poi_photo_url = poi_url + "/photo"
         yield Request(url=poi_photo_url, callback=self.parse_photo, meta={"poi_info": poi_info})
 
@@ -172,12 +196,13 @@ class QyerAlienSpotSpider(CrawlSpider):
         item["poi_englishName"] = poi_info["poi_englishName"]
         item["poi_summary"] = poi_info["poi_summary"]
         item["poi_detail"] = poi_info["poi_detail"]
-        poi_photo=sel.xpath('//div/ul[@class="pla_photolist clearfix"]/li/p[@class="pic"]/a/img/@src').extract()
+        poi_photo = sel.xpath('//div/ul[@class="pla_photolist clearfix"]/li/p[@class="pic"]/a/img/@src').extract()
         if poi_photo:
             item["poi_photo"] = []
             for url in poi_photo:
                 item["poi_photo"].append(url[:-7])
         yield item
+
 
 class QyerAlienPipeline(object):
     def process_item(self, item, spider):
