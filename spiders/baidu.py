60a3aff57d6c1d02c9385339c774e79950ad4160
Zephyre <haizi.zh@gmail.com>
2014-09-25 16:57:25 +0800

    MOD: 百度游记

--- spiders/baidu.py
+++ spiders/baidu.py
@@ -3,7 +3,6 @@ import json
 import os
 import random
 import re
-import urlparse
 import copy
 import hashlib
 import urllib2
@@ -12,7 +11,6 @@ import time
 
 import MySQLdb
 from MySQLdb.cursors import DictCursor
-
 import pymongo
 from scrapy import Request, Selector, log
 from scrapy.contrib.spiders import CrawlSpider
@@ -31,113 +29,65 @@ class BaiduNoteSpider(CrawlSpider):
         super(BaiduNoteSpider, self).__init__(*a, **kw)
 
     def start_requests(self):
-        col = pymongo.Connection().geo.Locality
-        url_base = 'http://lvyou.baidu.com/search/ajax/search?format=ajax&word=%s&pn=%d'
-        ret_list = list(col.find({'level': 2}, {'zhName': 1}))
-        for ret in ret_list:
-            url = url_base % (ret['zhName'], 0)
-            yield Request(url=url, callback=self.parse_loc,
-                          meta={'target': ret['zhName'], 'pn': 0, 'urlBase': url_base})
-            # yield Request(url='http://chanyouji.com/users/1', callback=self.parse)
+        start_locs = []
+        if 'param' in dir(self):
+            param = getattr(self, 'param', [])
+            if 'c' in param:
+                start_locs = param['c']
 
-            # yield Request(url='http://lvyou.baidu.com/notes/dc6b5c3d1354b88b3b405e2e/d-0', callback=self.parse,
-            # meta={'pageIdx': 0, 'noteId': 'dc6b5c3d1354b88b3b405e2e',
-            # 'urlBase': 'http://lvyou.baidu.com/notes/dc6b5c3d1354b88b3b405e2e/d-'})
+        if not start_locs:
+            col = pymongo.Connection().geo.Locality
+            start_locs = list(tmp['zhName'] for tmp in col.find({'level': 2}, {'zhName': 1}))
+
+        url_base = 'http://lvyou.baidu.com/search/ajax/search?format=ajax&word=%s&pn=%d&rn=10'
+
+        for ret in start_locs:
+            url = url_base % (ret, 0)
+            yield Request(url=url, callback=self.parse_loc,
+                          meta={'target': ret, 'pn': 0, 'urlBase': url_base})
 
     def parse_loc(self, response):
         target = response.meta['target']
-        pn = response.meta['pn'] + 10
         try:
             data = json.loads(response.body)
+
+            days_map = data['data']['search_res']['days']
+            costs_map = data['data']['search_res']['costs']
 
             if data['data']['search_res']['notes_list']:
                 # 读取下一页
                 url_base = response.meta['urlBase']
+                pn = response.meta['pn'] + 10
                 url = url_base % (target, pn)
                 yield Request(url=url, callback=self.parse_loc, meta={'target': target, 'pn': pn, 'urlBase': url_base})
+            else:
+                return
 
             url_base = 'http://lvyou.baidu.com/notes/%s/d-%d'
             for entry in data['data']['search_res']['notes_list']:
-                url = entry['loc']
-                m = re.search(r'/notes/([0-9a-f]+)', url)
-                if not m:
-                    continue
-                note_id = m.groups()[0]
-                url = url_base % (note_id, 0)
-                title = entry['title']
-                yield Request(url=url, callback=self.parse, meta={'title': title, 'target': target, 'pageIdx': 0,
-                                                                  'noteId': note_id, 'urlBase': url_base,
-                                                                  'note': {'url': url, 'summary': entry}})
+                url = url_base % (entry['nid'], 0)
+                entry['url'] = url
+                entry['target'] = target
+
+                cost_id = entry['avg_cost']
+                ret = filter(lambda val: val['id'] == cost_id, costs_map)
+                if ret:
+                    ret = ret[0]
+                    entry['lower_cost'] = int(ret['buildrange'][0])
+                    entry['upper_cost'] = int(ret['buildrange'][0])
+
+                yield Request(url=url, callback=self.parse,
+                              meta={'target': target, 'pageIdx': 0, 'urlBase': url_base, 'note': entry,
+                                    'days': days_map, 'costs': costs_map})
 
         except (ValueError, KeyError, TypeError):
             pass
 
     def parse(self, response):
         note = response.meta['note'] if 'note' in response.meta else {}
-        note_id = response.meta['noteId']
         page_idx = response.meta['pageIdx']
+        note_id = note['nid']
         sel = Selector(response)
-
-        if page_idx == 0:
-            note['id'] = response.meta['noteId']
-            note['target'] = response.meta['target']
-            note['title'] = response.meta['title']
-            # # 标题
-            # ret = sel.xpath('//span[@id="J_notes-title"]/text()').extract()
-            # if ret:
-            # tmp = ret[0].strip()
-            # if tmp:
-            # note['title'] = tmp
-
-            ret = sel.xpath('//ul[@id="J_basic-info-container"]')
-            info_node = ret[0] if ret else None
-            if info_node:
-                ret = info_node.xpath('./li/span[contains(@class, "author-icon")]/a')
-                if ret:
-                    user_node = ret[0]
-                    note['authorName'] = user_node.xpath('./text()').extract()[0]
-                    tmp = user_node.xpath('./@href').extract()[0]
-                    m = re.compile(r'[^/]+$').search(tmp)
-                    if m:
-                        note['authorId'] = m.group()
-
-                ret = info_node.xpath('./li//span[contains(@class, "start_time")]/text()').extract()
-                if ret:
-                    tmp = ret[0].strip()
-                    if tmp:
-                        note['startTime'] = tmp
-
-                ret = info_node.xpath('./li/span[contains(@class, "path-icon")]/span[@class="infos"]/text()').extract()
-                if ret and len(ret) == 2:
-                    tmp = ret[0].strip()
-                    m = re.search(ur'^从(.+)', tmp)
-                    if m:
-                        note['fromLoc'] = m.groups()[0]
-                    tmp = ret[1].strip()
-                    if tmp:
-                        note['toLoc'] = filter(lambda val: val and not re.match(ur'^\s*\.+\s*$', val),
-                                               list(tmp.strip() for tmp in tmp.split(u'、')))
-                ret = info_node.xpath('./li/span[contains(@class, "time-icon")]/span[@class="infos"]/text()').extract()
-                if ret:
-                    m = re.search(ur'(\d+)天', ret[0].strip())
-                    if m:
-                        note['timeCost'] = int(m.groups()[0])
-
-                ret = info_node.xpath('./li/span[contains(@class, "cost-icon")]/span[@class="infos"]/text()').extract()
-                if ret:
-                    tmp = ret[0].strip()
-                    if tmp:
-                        note['cost'] = tmp
-
-                ret = info_node.xpath('./li[contains(@class, "notes-info-foot")]/div[@class="fl"]/text()').extract()
-                if ret:
-                    tmp = ret[0].strip()
-                    m = re.search(ur'回\s*复\s*(\d+)', tmp)
-                    if m:
-                        note['replyCnt'] = int(m.groups()[0])
-                    m = re.search(ur'浏\s*览\s*(\d+)', tmp)
-                    if m:
-                        note['viewCnt'] = int(m.groups()[0])
 
         if 'contents' not in note:
             note['contents'] = []
@@ -150,9 +100,13 @@ class BaiduNoteSpider(CrawlSpider):
         # 是否存在下一页？
         tmp = sel.xpath('//span[@id="J_notes-view-pagelist"]/a[@class="nslog"]/text()').extract()
         if not tmp or not contents_list:
-            url_t = 'http://lvyou.baidu.com/notes/%s-%d'
-            url = url_t % (note['id'], len(note['contents']))
-            yield Request(url=url, callback=self.parse_comments, meta={'urlT': url_t, 'note': note})
+            item = BaiduNoteItem()
+            item['note'] = note
+            yield item
+            #
+            # url_t = 'http://lvyou.baidu.com/notes/%s-%d'
+            # url = url_t % (note['nid'], len(note['contents']))
+            # yield Request(url=url, callback=self.parse_comments, meta={'urlT': url_t, 'note': note})
         else:
             page_idx += 1
             url_base = response.meta['urlBase']
@@ -160,56 +114,56 @@ class BaiduNoteSpider(CrawlSpider):
             yield Request(url=url, callback=self.parse,
                           meta={'pageIdx': page_idx, 'noteId': note_id, 'urlBase': url_base, 'note': note})
 
-    def parse_comments(self, response):
-        note = response.meta['note']
-
-        if 'comments' not in note:
-            note['comments'] = []
-        comments = note['comments']
-        author = note['authorName']
-
-        sel = Selector(response)
-
-        node_list = sel.xpath('//div[@id="building-container"]//div[contains(@class, "grid-s5m0")]')
-        for node in node_list:
-            ret = node.xpath('./div[@class="col-main"]/div[@class="floor"]/div[@class="floor-content"]')
-            if not ret:
-                continue
-            c_node = ret[0]
-            ret = c_node.xpath('./@nickname').extract()
-            if not ret or (ret[0] == author and not comments):
-                continue
-            c_author = ret[0]
-            ret = c_node.xpath('./@uid').extract()
-            if not ret:
-                continue
-            c_author_id = ret[0]
-
-            tmp = c_node.extract()
-            if tmp:
-                comments.append({'authorName': c_author, 'authorId': c_author_id, 'comment': tmp})
-
-        # 检查是否有下一页
-        tmp = sel.xpath('//span[@id="J_notes-view-pagelist"]/a[@class="nslog"]/text()').extract()
-        tmp = tmp[-1] if tmp else None
-        if tmp:
-            try:
-                tmp = int(tmp)
-            except ValueError:
-                tmp = None
-
-        if not tmp:
-            tmp_href = sel.xpath('//span[@id="J_notes-view-pagelist"]/a[@class="nslog"]/@href').extract()
-            if tmp_href:
-                href = tmp_href[-1]
-                parts = urlparse.urlparse(response.url)
-                url = urlparse.urlunparse((parts[0], parts[1], href, '', '', ''))
-                return Request(url=url, callback=self.parse_comments,
-                               meta={'urlT': response.meta['urlT'], 'note': note})
-
-        item = BaiduNoteItem()
-        item['note'] = note
-        return item
+            # def parse_comments(self, response):
+            # note = response.meta['note']
+            #
+            # if 'comments' not in note:
+            #         note['comments'] = []
+            #     comments = note['comments']
+            #     author = note['authorName']
+            #
+            #     sel = Selector(response)
+            #
+            #     node_list = sel.xpath('//div[@id="building-container"]//div[contains(@class, "grid-s5m0")]')
+            #     for node in node_list:
+            #         ret = node.xpath('./div[@class="col-main"]/div[@class="floor"]/div[@class="floor-content"]')
+            #         if not ret:
+            #             continue
+            #         c_node = ret[0]
+            #         ret = c_node.xpath('./@nickname').extract()
+            #         if not ret or (ret[0] == author and not comments):
+            #             continue
+            #         c_author = ret[0]
+            #         ret = c_node.xpath('./@uid').extract()
+            #         if not ret:
+            #             continue
+            #         c_author_id = ret[0]
+            #
+            #         tmp = c_node.extract()
+            #         if tmp:
+            #             comments.append({'authorName': c_author, 'authorId': c_author_id, 'comment': tmp})
+            #
+            #     # 检查是否有下一页
+            #     tmp = sel.xpath('//span[@id="J_notes-view-pagelist"]/a[@class="nslog"]/text()').extract()
+            #     tmp = tmp[-1] if tmp else None
+            #     if tmp:
+            #         try:
+            #             tmp = int(tmp)
+            #         except ValueError:
+            #             tmp = None
+            #
+            #     if not tmp:
+            #         tmp_href = sel.xpath('//span[@id="J_notes-view-pagelist"]/a[@class="nslog"]/@href').extract()
+            #         if tmp_href:
+            #             href = tmp_href[-1]
+            #             parts = urlparse.urlparse(response.url)
+            #             url = urlparse.urlunparse((parts[0], parts[1], href, '', '', ''))
+            #             return Request(url=url, callback=self.parse_comments,
+            #                            meta={'urlT': response.meta['urlT'], 'note': note})
+            #
+            #     item = BaiduNoteItem()
+            #     item['note'] = note
+            #     return item
 
 
 class BaiduNotePipeline(object):
@@ -217,8 +171,8 @@ class BaiduNotePipeline(object):
         if not isinstance(item, BaiduNoteItem):
             return item
 
-        col = pymongo.Connection().raw_notes.BaiduNote
-        ret = col.find_one({'id': item['note']['id']}, {'_id': 1})
+        col = pymongo.Connection().raw_data.BaiduNote
+        ret = col.find_one({'nid': item['note']['nid']}, {'_id': 1})
         if ret:
             item['note']['_id'] = ret['_id']
 
