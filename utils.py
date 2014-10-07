9cd7ab6df761b45a09602d019414af668459b77c
Zephyre <haizi.zh@gmail.com>
2014-10-07 12:11:42 +0800

    ADD: 根据locality全称，获得简称。比如：重庆市->重庆，海西蒙古族自治州->海西等。

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 from math import radians, cos, sin, asin, sqrt
+import re
 
 import pymongo
 
@@ -53,3 +54,38 @@ def haversine(lon1, lat1, lon2, lat2):
     # 6367 km is the radius of the Earth
     km = 6367 * c
     return km
+
+
+def get_short_loc(name):
+    """
+    根据locality全称，获得简称。比如：重庆市->重庆，海西蒙古族自治州->海西等。
+    :param name:
+    """
+    match = re.search(ur'(族|自治)', name)
+    if match:
+        while True:
+            stripped = False
+
+            name = name[:match.start()]
+            idx = len(name) - 1
+            while idx > 1:
+                minor_cand = name[idx:]
+                if minor_cand[-1] != u'族':
+                    minor_cand += u'族'
+                if '|' + minor_cand + '|' in u'|汉族|壮族|满族|回族|苗族|维吾尔族|土家族|彝族|蒙古族|藏族|布依族|侗族|瑶族|朝鲜族|白族|哈尼族|哈萨克族|黎族|傣族|畲族|傈僳族|仡佬族|东乡族|高山族|拉祜族|水族|佤族|纳西族|羌族|土族|仫佬族|锡伯族|柯尔克孜族|达斡尔族|景颇族|毛南族|撒拉族|布朗族|塔吉克族|阿昌族|普米族|鄂温克族|怒族|京族|基诺族|德昂族|保安族|俄罗斯族|裕固族|乌兹别克族|门巴族|鄂伦春族|独龙族|塔塔尔族|赫哲族|珞巴族|':
+                    name = name[:idx]
+                    stripped = True
+                    break
+                else:
+                    idx -= 1
+
+            if not stripped:
+                break
+
+    match = re.search(ur'(市|县|省|直辖市|自治县|自治区|地区)$', name)
+    if match:
+        tmp = name[:match.start()]
+        if len(tmp) > 1:
+            name = tmp
+
+    return name
