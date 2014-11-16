50ef68b71f98264ed491b6951c51df0743a0fa74
海大富 <haizi.zh@gmail.com>
2014-11-17 01:23:32 +0800

    MOD: 改进蚂蜂窝爬虫

--- spiders/mafengwo.py
+++ spiders/mafengwo.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+import json
 import re
 
 from scrapy import Item, Request, Field, Selector, log
@@ -11,7 +12,6 @@ __author__ = 'zephyre'
 
 class MafengwoMddItem(Item):
     data = Field()
-    type = Field()
 
 
 class MafengwoMddSpider(AizouCrawlSpider):
@@ -27,20 +27,69 @@ class MafengwoMddSpider(AizouCrawlSpider):
 
     def start_requests(self):
         urls = [
-            'http://www.mafengwo.cn/jd/52314/',  # 亚洲
-            'http://www.mafengwo.cn/jd/10853/gonglve.html',  # 南极州
-            'http://www.mafengwo.cn/jd/14701/gonglve.html',  # 大洋洲
-            'http://www.mafengwo.cn/jd/14517/gonglve.html',  # 非洲
-            'http://www.mafengwo.cn/jd/14383/gonglve.html',  # 欧洲
-            'http://www.mafengwo.cn/jd/16406/gonglve.html',  # 南美
-            'http://www.mafengwo.cn/jd/16867/gonglve.html',  # 北美
+            'http://www.mafengwo.cn/jd/52314/gonglve.html',  # 亚洲
+            'http://www.mafengwo.cn/jd/10853/',  # 南极州
+            'http://www.mafengwo.cn/jd/14701/',  # 大洋洲
+            'http://www.mafengwo.cn/jd/14517/',  # 非洲
+            'http://www.mafengwo.cn/jd/14383/',  # 欧洲
+            'http://www.mafengwo.cn/jd/16406/',  # 南美
+            'http://www.mafengwo.cn/jd/16867/',  # 北美
         ]
         return [Request(url=url) for url in urls]
 
     def parse(self, response):
+        self.param = getattr(self, 'param', {})
+
+        if 'region' in self.param:
+            region_list = [int(tmp) for tmp in self.param['region']]
+        else:
+            region_list = None
+
         for node in Selector(response).xpath('//dd[@id="region_list"]/a[@href]'):
             url = self.build_href(response.url, node.xpath('./@href').extract()[0])
-            yield Request(url=url, callback=self.parse_mdd, meta={'type': 'region'})
+            mdd_id = int(re.search(r'mafengwo\.cn/jd/(\d+)', url).group(1))
+            if region_list and mdd_id not in region_list:
+                continue
+
+            url = 'http://www.mafengwo.cn/travel-scenic-spot/mafengwo/%d.html' % mdd_id
+            yield Request(url=url, callback=self.parse_mdd_home, meta={'id': mdd_id})
+
+    def parse_mdd_home(self, response):
+        item = MafengwoMddItem()
+        data = {'id': response.meta['id']}
+        item['data'] = data
+
+        crumb = self.get_crumb(response)
+        data['crumb'] = crumb
+        for crumb_url in [tmp['url'] for tmp in crumb]:
+            mdd_id = int(re.search(r'travel-scenic-spot/mafengwo/(\d+).html', crumb_url).group(1))
+            yield Request(url=crumb_url, callback=self.parse_mdd_home, meta={'id': mdd_id})
+
+        sel = Selector(response)
+        for node1 in sel.xpath('//div[contains(@class,"m-tags")]/div[@class="bd"]/div[@class="t-info"]/p'):
+            span_list = node1.xpath('./span/text()').extract()
+            if len(span_list) < 2 and len(span_list) > 0:
+                self.log('Unsupported region homepage: %d' % data['id'], log.WARNING)
+                continue
+            elif not span_list:
+                continue
+
+            hdr = span_list[0].strip()
+            contents = '\n'.join([tmp.strip() for tmp in span_list[1:]])
+            if hdr == u'最佳旅游季节':
+                data['travel_month'] = contents
+            elif hdr == u'建议游玩天数':
+                data['time_cost'] = contents
+            else:
+                self.log('Unsupported region homepage: %d' % data['id'], log.WARNING)
+                continue
+
+        tags = sel.xpath(
+            '//div[contains(@class,"m-tags")]/div[@class="bd"]/ul/li[@class="impress-tip"]/a[@href]/text()').extract()
+        data['tags'] = list(set(filter(lambda val: val, [tmp.strip() for tmp in tags])))
+
+        url = 'http://www.mafengwo.cn/jd/%d/gonglve.html' % data['id']
+        yield Request(url=url, callback=self.parse_jd, meta={'item': item, 'type': 'region'})
 
     def parse_poi_list(self, response):
         """
@@ -48,19 +97,38 @@ class MafengwoMddSpider(AizouCrawlSpider):
         :param response:
         :return:
         """
-        poi_type = response.meta['type']
+        poi_type = response.meta['poi_type']
         for href in Selector(response).xpath(
                 '//ul[@class="poi-list"]/li[contains(@class,"item")]/div[@class="title"]//a[@href]/@href').extract():
-            yield Request(self.build_href(response.url, href), meta={'type': poi_type}, callback=self.parse_poi)
+            yield Request(self.build_href(response.url, href), meta={'poi_type': poi_type}, callback=self.parse_poi)
 
-    def parse_mdd(self, response):
+    def get_crumb(self, response):
+        # 获得crumb
+        crumb = []
+        for node in Selector(response).xpath(
+                '//div[@class="crumb"]/div[contains(@class,"item")]/div[@class="drop"]/span[@class="hd"]/a[@href]'):
+            crumb_name = node.xpath('./text()').extract()[0].strip()
+            crumb_url = self.build_href(response.url, node.xpath('./@href').extract()[0])
+            if not re.search(r'travel-scenic-spot/mafengwo/\d+\.html', crumb_url):
+                continue
+            crumb.append({'name': crumb_name, 'url': crumb_url})
+        return crumb
+
+    def parse_jd(self, response):
         sel = Selector(response)
+
         ctype = response.meta['type']
+        # 如果是目的地页面，其下的所有poi都为景点类型。否则，poi_type和ctype一致，比如都为gw, cy等
+        poi_type = 'vs' if ctype == 'region' else ctype
+        response.meta['poi_type'] = poi_type
 
         # 继续抓取下级的region
-        for node in sel.xpath('//dd[@id="region_list"]/a[@href]'):
-            url = self.build_href(response.url, node.xpath('./@href').extract()[0])
-            yield Request(url=url, callback=self.parse_mdd, meta={'type': ctype})
+        if ctype == 'region':
+            for node in sel.xpath('//dd[@id="region_list"]/a[@href]'):
+                url = self.build_href(response.url, node.xpath('./@href').extract()[0])
+                mdd_id = int(re.search(r'mafengwo\.cn/jd/(\d+)', url).group(1))
+                url = 'http://www.mafengwo.cn/travel-scenic-spot/mafengwo/%d.html' % mdd_id
+                yield Request(url=url, callback=self.parse_mdd_home, meta={'type': ctype, 'id': mdd_id})
 
         results = self.parse_poi_list(response)
         if hasattr(results, '__iter__'):
@@ -71,71 +139,54 @@ class MafengwoMddSpider(AizouCrawlSpider):
 
         # poi列表的翻页
         for href in sel.xpath('//div[@class="page-hotel"]/a[@href]/@href').extract():
-            yield Request(self.build_href(response.url, href), callback=self.parse_poi_list, meta={'type': ctype})
+            yield Request(self.build_href(response.url, href), callback=self.parse_poi_list,
+                          meta={'poi_type': poi_type})
 
-        # 根据url判断是否为poi
-        if re.search(r'poi/\d+.html', response.url):
-            results = self.parse_poi(response)
-            if hasattr(results, '__iter__'):
-                for entry in results:
-                    yield entry
-            elif isinstance(results, Request):
-                yield results
-            return
+        if ctype == 'region':
+            item = response.meta['item']
+            data = item['data']
 
-        item = MafengwoMddItem()
-        data = {'id': int(re.search(r'/(\d+)(/gonglve.html|/)?$', response.url).group(1))}
-        item['data'] = data
-        item['type'] = ctype
+            col_list = []
+            for node in sel.xpath('//ul[@class="nav-box"]/li[contains(@class,"nav-item")]/a[@href]'):
+                info_title = node.xpath('./text()').extract()[0]
+                next_url = self.build_href(response.url, node.xpath('./@href').extract()[0])
+                # 根据是否出现国家攻略来判断是否为国家
+                if info_title == u'国家概况' or info_title == u'城市概况':
+                    col_list.extend(
+                        [(self.build_href(response.url, tmp), self.parse_info, {'type': 'region'}) for tmp in
+                         node.xpath('..//dl/dt/a[@href]/@href').extract()])
+                    if info_title == u'国家概况':
+                        data['type'] = 'country'
+                    elif info_title == u'城市概况':
+                        data['type'] = 'region'
+                    else:
+                        self.log(u'Invalid MDD type: %s, %s' % (info_title, response.url), log.WARNING)
+                        return
 
-        # 获得crumb
-        crumb = []
-        for node in sel.xpath(
-                '//div[@class="crumb"]/div[contains(@class,"item")]/div[@class="drop"]/span[@class="hd"]/a[@href]'):
-            crumb_name = node.xpath('./text()').extract()[0].strip()
-            crumb_url = self.build_href(response.url, node.xpath('./@href').extract()[0])
-            if not re.search(r'travel-scenic-spot/mafengwo/\d+\.html', crumb_url):
-                continue
-            crumb.append({'name': crumb_name, 'url': crumb_url})
-        data['crumb'] = crumb
+                elif info_title == u'购物':
+                    yield Request(url=next_url, callback=self.parse_jd, meta={'type': 'gw'})
+                elif info_title == u'娱乐':
+                    yield Request(url=next_url, callback=self.parse_jd, meta={'type': 'yl'})
+                elif info_title == u'美食':
+                    yield Request(url=next_url, callback=self.parse_jd, meta={'type': 'cy'})
 
-        col_list = []
-        for node in sel.xpath('//ul[@class="nav-box"]/li[contains(@class,"nav-item")]/a[@href]'):
-            info_title = node.xpath('./text()').extract()[0]
-            next_url = self.build_href(response.url, node.xpath('./@href').extract()[0])
-            # 根据是否出现国家攻略来判断是否为国家
-            if info_title == u'国家概况' or info_title == u'城市概况':
-                col_list.extend(
-                    [(self.build_href(response.url, tmp), self.parse_info, {'type': 'region'}) for tmp in
-                     node.xpath('..//dl/dt/a[@href]/@href').extract()])
-                if info_title == u'国家概况':
-                    data['type'] = 'country'
-                elif info_title == u'城市概况':
-                    data['type'] = 'region'
-                else:
-                    self.log(u'Invalid MDD type: %s, %s' % (info_title, response.url), log.WARNING)
-                    return
-
-            elif info_title == u'购物':
-                yield Request(url=next_url, callback=self.parse_mdd, meta={'type': 'gw'})
-            elif info_title == u'娱乐':
-                yield Request(url=next_url, callback=self.parse_mdd, meta={'type': 'yl'})
-            elif info_title == u'美食':
-                yield Request(url=next_url, callback=self.parse_mdd, meta={'type': 'cy'})
-
-        col_url, cb, meta = col_list[0]
-        col_list = col_list[1:]
-        meta['col_list'] = col_list
-        meta['item'] = item
-        yield Request(url=col_url, callback=cb, meta=meta)
-
-    def parse_poi(self, response):
-        return []
+            col_url, cb, meta = col_list[0]
+            col_list = col_list[1:]
+            meta['col_list'] = col_list
+            meta['item'] = item
+            yield Request(url=col_url, callback=cb, meta=meta)
 
     def parse_info(self, response):
         sel = Selector(response)
         item = response.meta['item']
         data = item['data']
+
+        if 'title' not in data:
+            tmp = sel.xpath(
+                '//div[contains(@class,"sub-nav")]/div[@class="mdd-title"]/span[@class="s-name"]/text()').extract()[
+                0].strip()
+            m = re.search(ur'(.+)(城市|国家)概况', tmp)
+            data['title'] = m.group(1).strip()
 
         if 'contents' not in data:
             data['contents'] = []
@@ -152,7 +203,6 @@ class MafengwoMddSpider(AizouCrawlSpider):
                 continue
         contents.append(entry)
         data['contents'] = filter(lambda val: val, contents)
-
         col_list = response.meta['col_list']
         if col_list:
             col_url, cb, meta = col_list[0]
@@ -161,4 +211,106 @@ class MafengwoMddSpider(AizouCrawlSpider):
             meta['item'] = item
             yield Request(url=col_url, callback=cb, meta=meta)
         else:
+            # 开始抓取图像
+            url = 'http://www.mafengwo.cn/mdd/ajax_photolist.php?act=getMddPhotoList&mddid=%d&page=1' % data['id']
+            yield Request(url=url, callback=self.parse_photo, meta={'item': item, 'page': 1, 'act': 'getMddPhotoList'})
+
+    def parse_poi(self, response):
+        sel = Selector(response)
+        crumb = self.get_crumb(response)
+        for crumb_url in [tmp['url'] for tmp in crumb]:
+            mdd_id = int(re.search(r'travel-scenic-spot/mafengwo/(\d+).html', crumb_url).group(1))
+            yield Request(url=crumb_url, callback=self.parse_mdd_home, meta={'type': 'region', 'id': mdd_id})
+
+        title = sel.xpath('//div[contains(@class,"m-details")]/div[contains(@class,"title")]//h1/text()').extract()[
+            0].strip()
+
+        pid = int(re.search('mafengwo\.cn/poi/(\d+)\.html', response.url).group(1))
+
+        loc_data = json.loads('{%s}' % re.search(r'window\.Env\s*=\s*\{(.+?)\}\s*;', response.body).group(1))
+        lat = loc_data['lat']
+        lng = loc_data['lng']
+
+        score = float(
+            sel.xpath('//div[@class="txt-l"]/div[@class="score"]/span[@class="score-info"]/em/text()').extract()[0]) / 5
+        comment_cnt = int(
+            sel.xpath('//div[@class="txt-l"]/div[@class="score"]/p[@class="ranking"]/em/text()').extract()[0])
+        tmp = sel.xpath('//div[@class="pic-r"]/a[@href]/span[@class="pic-num"]/text()').extract()[0]
+        photo_cnt = int(re.search(ur'共(\d+)张', tmp).group(1))
+
+        desc = []
+        desc_entry = {}
+        for node in sel.xpath('//div[contains(@class,"poi-info")]/div[@class="bd"]/*'):
+            node_str = node.extract()
+            node_name = re.search(r'^\s*<([^<>]+)>', node_str).group(1).strip()
+            if node_name == 'h3':
+                desc.append(desc_entry)
+                desc_entry = {'name': node.xpath('./text()').extract()[0].strip()}
+            elif node_name == 'p':
+                desc_entry['contents'] = node_str
+            else:
+                assert False
+        desc.append(desc_entry)
+        desc = filter(lambda val: val, desc)
+
+        data = {'id': pid, 'type': response.meta['poi_type'], 'title': title, 'crumb': crumb, 'rating': score,
+                'comment_cnt': comment_cnt, 'images_tot': photo_cnt, 'desc': desc, 'lat': lat, 'lng': lng}
+        item = MafengwoMddItem()
+        item['data'] = data
+
+        url = 'http://www.mafengwo.cn/mdd/ajax_photolist.php?act=getPoiPhotoList&poiid=%d&page=1' % pid
+        yield Request(url=url, meta={'item': item, 'page': 1, 'act': 'getPoiPhotoList'}, callback=self.parse_photo)
+
+    def parse_photo(self, response):
+        sel = Selector(response)
+        item = response.meta['item']
+        page = response.meta['page']
+        data = item['data']
+
+        images = data['images'] if 'images' in data else []
+        data['images'] = images
+        node_list = sel.xpath('//ul/li')
+        # 最多去50张照片左右
+        if not node_list or len(images) > 200:
+            # 已经到尽头
             yield item
+        else:
+            for node in node_list:
+                img_node = node.xpath('./a[@href]/img')[0]
+                img_url = self.build_href(response.url, img_node.xpath('./@src').extract()[0])
+                img_url = re.sub(r'\.[^\.]+\.w\d+\.', '.', img_url)
+                tmp = img_node.xpath('./@title').extract()
+                if tmp and tmp[0].strip():
+                    title = tmp[0].strip()
+                else:
+                    title = None
+
+                try:
+                    favor_cnt = int(node.xpath('./span[contains(@class,"num-like")]/text()').extract()[0])
+                except (IndexError, ValueError):
+                    favor_cnt = 0
+
+                image = {'url': img_url, 'favor_cnt': favor_cnt}
+                if title:
+                    image['title'] = title
+
+                tmp = img_node.xpath('./div[contains(@class,"info")]/a[@href]')
+                if tmp:
+                    user_node = tmp[0]
+                    user_url = self.build_href(response.url, user_node.xpath('./@href').extract()[0])
+                    user_id = int(re.search(r'/u/(\d+)\.html', user_url).group(1))
+                    user_name = user_node.xpath('./text()').extract()[0].strip()
+                    image['user_id'] = user_id,
+                    image['user_name'] = user_name
+
+                if img_url not in [tmp['url'] for tmp in images]:
+                    images.append(image)
+
+            page += 1
+            act = response.meta['act']
+            param_name = 'mddid' if act == 'getMddPhotoList' else 'poiid'
+            url = 'http://www.mafengwo.cn/mdd/ajax_photolist.php?act=%s&%s=%d&page=%d' % (
+                act, param_name, data['id'], page)
+            yield Request(url=url, meta={'item': item, 'page': page, 'act': act}, callback=self.parse_photo)
+
+
