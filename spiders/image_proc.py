139e06f205f651a2c6e481426251e48120bd5f99
海大富 <haizi.zh@gmail.com>
2014-12-03 14:14:25 +0800

    MOD: 全新的照片抓取爬虫

--- spiders/image_proc.py
+++ spiders/image_proc.py
@@ -20,16 +20,7 @@ __author__ = 'zephyre'
 
 class ImageProcItem(Item):
     # define the fields for your item here like:
-    db = Field()
-    col = Field()
-    list1 = Field()
-    list1_name = Field()
-    list2 = Field()
-    list2_name = Field()
-    doc_id = Field()
-    stat = Field()
-    image_info = Field()
-    is_done = Field()
+    image = Field()
 
 
 class AlbumProcItem(Item):
@@ -128,75 +119,23 @@ class ImageProcSpider(AizouCrawlSpider):
     def parse(self, response):
         db = self.param['db'][0]
         col_name = self.param['col'][0]
-        list1_name = self.param['from'][0] if 'from' in self.param else 'imageList'
-        list2_name = self.param['to'][0] if 'to' in self.param else 'images'
         profile = self.param['profile'][0] if 'profile' in self.param else 'mongodb-general'
 
         col = self.fetch_db_col(db, col_name, profile)
-        col_im = self.fetch_db_col('imagestore', 'Images', 'mongodb-general')
+        col_im_c = self.fetch_db_col('imagestore', 'ImageCandidates', 'mongodb-general')
 
-        for entry in col.find({list1_name: {'$ne': None}}, {list1_name: 1, list2_name: 1, 'isDone': 1}):
-            # 从哪里取原始url？比如：imageList
-            list1 = entry[list1_name] if list1_name in entry else []
+        query = json.loads(self.param['query'][0]) if 'query' in self.param else {}
+        cursor = col.find(query, {'_id': 1})
 
-            # list1有两种格式，一种是原始的plain string list，另一种是带有metadata的，形如：[{'url': '', 'author': ''}]
-            # 这里需要做转换
-            if list1 and (isinstance(list1[0], str) or isinstance(list1[0], unicode)):
-                list1 = [{'url': tmp} for tmp in list1]
+        if 'limit' in self.param:
+            cursor.limit(int(self.param['limit'][0]))
 
-            # 往哪里存？默认：images
-            list2 = entry[list2_name] if list2_name in entry else []
-
-            item = ImageProcItem()
-            item['doc_id'] = entry['_id']
-            item['db'] = db
-            item['col'] = col_name
-            item['list1'] = list1
-            item['list1_name'] = list1_name
-            item['list2'] = list2
-            item['list2_name'] = list2_name
-            item['is_done'] = entry['isDone'] if 'isDone' in entry else False
-
-            upload_list = []
-            url_set = set([tmp['url'] for tmp in list2])
-            for list1_entry in list1:
-                url = list1_entry['url']
-                url1 = 'http://lvxingpai-img-store.qiniudn.com/assets/images/%s' % hashlib.md5(url).hexdigest()
-                if url in url_set or url1 in url_set:
-                    continue
-
-                # 原始的元数据
-                img_meta = {k.encode('utf-8'): list1_entry[k] for k in list1_entry if k != 'url'}
-
-                # 是否已经在数据库中存在
-                match = re.search(r'http://lvxingpai-img-store\.qiniudn\.com/(.+)', url)
-                if match:
-                    image = col_im.find_one({'key': match.groups()[0]})
-                else:
-                    image = col_im.find_one({'url_hash': hashlib.md5(url).hexdigest()})
-
-                if image:
-                    url2 = 'http://lvxingpai-img-store.qiniudn.com/' + image['key']
-                    if url2 not in url_set:
-                        tmp = {'url': url2, 'h': image['h'], 'w': image['w'], 'fSize': image['size'],
-                               'enabled': True}
-                        for k in img_meta:
-                            if k not in tmp:
-                                tmp[k] = img_meta[k]
-                        list2.append(tmp)
-                else:
-                    # 是否下载缺失的图像
-                    if 'skip-upload' not in self.param:
-                        upload_list.append((url, img_meta))
-
-            if not upload_list:
-                yield item
-            else:
-                # 开始链式下载
-                url, img_meta = upload_list[0]
-                upload_list = upload_list[1:]
-                yield Request(url=url, meta={'src': url, 'item': item, 'upload': upload_list, 'img_meta': img_meta},
-                              headers={'Referer': None}, callback=self.parse_img)
+        for entry in cursor:
+            for img in col_im_c.find({'itemIds': entry['_id']}):
+                item = ImageProcItem()
+                item['image'] = img
+                url = img['url']
+                yield Request(url=url, meta={'item': item}, headers={'Referer': None}, callback=self.parse_img)
 
     def get_upload_token(self, key, bucket='lvxingpai-img-store', overwrite=True):
         """
@@ -221,6 +160,7 @@ class ImageProcSpider(AizouCrawlSpider):
     def parse_img(self, response):
         if response.status not in [400, 403, 404]:
             self.log('DOWNLOADED: %s' % response.url, log.INFO)
+            meta = response.meta
 
             fname = './tmp/%d' % (long(time.time() * 1000) + random.randint(1, 10000))
             with open(fname, 'wb') as f:
@@ -228,11 +168,9 @@ class ImageProcSpider(AizouCrawlSpider):
 
             if not self.check_img(fname):
                 os.remove(fname)
-                for entry in self.next_proc(response):
-                    yield entry
+                return
             else:
-                key = 'assets/images/%s' % hashlib.md5(response.meta['src']).hexdigest()
-
+                key = 'assets/images/%s' % meta['item']['image']['url_hash']
                 sc = False
                 self.log('START UPLOADING: %s <= %s' % (key, response.url), log.INFO)
 
@@ -260,95 +198,46 @@ class ImageProcSpider(AizouCrawlSpider):
                 # 统计信息
                 bucket = 'lvxingpai-img-store'
                 url = 'http://%s.qiniudn.com/%s?stat' % (bucket, key)
-                meta = response.meta
-                yield Request(url=url, meta={'src': meta['src'], 'item': meta['item'], 'upload': meta['upload'],
-                                             'key': key, 'bucket': bucket, 'img_meta': meta['img_meta']},
+                yield Request(url=url, meta={'item': meta['item'], 'key': key, 'bucket': bucket},
                               callback=self.parse_stat)
-        else:
-            for entry in self.next_proc(response):
-                yield entry
-
-    def next_proc(self, response):
-        """
-        放弃当前的item chain，处理下一个item
-        :param response:
-        """
-        meta = response.meta
-        upload = meta['upload']
-        item = meta['item']
-        # 从list1中去掉这个url
-        list1 = item['list1']
-        item['list1'] = filter(lambda val: val['url'] != meta['src'], list1)
-
-        if not upload:
-            yield item
-        else:
-            url, img_meta = upload[0]
-            upload = upload[1:]
-            yield Request(url=url, meta={'src': url, 'item': item, 'upload': upload, 'img_meta': img_meta},
-                          headers={'Ref erer': None}, callback=self.parse_img)
 
     def parse_stat(self, response):
         stat = json.loads(response.body)
         meta = response.meta
         item = meta['item']
-        upload = meta['upload']
         key = meta['key']
         bucket = meta['bucket']
-        src = meta['src']
 
         url = 'http://%s.qiniudn.com/%s?imageInfo' % (bucket, key)
-        yield Request(url=url, meta={'item': item, 'upload': upload, 'key': key, 'bucket': bucket, 'stat': stat,
-                                     'img_meta': meta['img_meta'], 'src': src}, callback=self.parse_image_info)
+        yield Request(url=url, callback=self.parse_image_info,
+                      meta={'item': item, 'key': key, 'bucket': bucket, 'stat': stat})
 
     def parse_image_info(self, response):
         image_info = json.loads(response.body)
-        if 'error' in image_info:
-            for entry in self.next_proc(response):
-                yield entry
-        else:
+        if 'error' not in image_info:
             meta = response.meta
             item = meta['item']
-            upload = meta['upload']
             key = meta['key']
-            bucket = meta['bucket']
             stat = meta['stat']
-            src = meta['src']
-            img_meta = meta['img_meta']
 
-            entry = {'url_hash': hashlib.md5(src).hexdigest(),
+            img = item['image']
+            entry = {'url_hash': hashlib.md5(img['url']).hexdigest(),
                      'cTime': long(time.time() * 1000),
                      'cm': image_info['colorModel'],
                      'h': image_info['height'],
                      'w': image_info['width'],
                      'fmt': image_info['format'],
                      'size': stat['fsize'],
-                     'url': src,
+                     'url': img['url'],
                      'key': key,
                      'type': stat['mimeType'],
                      'hash': stat['hash']}
-            for k, v in img_meta.items():
-                if k not in entry:
-                    entry[k] = v
-            col_im = self.fetch_db_col('imagestore', 'Images', 'mongodb-general')
-            col_im.save(entry)
+            for k, v in entry.items():
+                img[k] = v
+            if '_id' in img:
+                img.pop('_id')
 
-            # 修正list
-            item['list2'].append({
-                'url': 'http://%s.qiniudn.com/%s' % (bucket, key),
-                'h': image_info['height'],
-                'w': image_info['width'],
-                'fSize': stat['fsize'],
-                'enabled': True
-            })
-
-            if not upload:
-                yield item
-            else:
-                url, img_meta = upload[0]
-                upload = upload[1:]
-                yield Request(url=url, meta={'src': url, 'item': item, 'upload': upload, 'img_meta': img_meta},
-                              headers={'Ref erer': None}, callback=self.parse_img)
+            yield item
 
 
 class ImageProcPipeline(AizouPipeline):
@@ -362,74 +251,19 @@ class ImageProcPipeline(AizouPipeline):
         if not self.is_handler(item, spider):
             return item
 
-        db = item['db']
-        col_name = item['col']
-        list1_name = item['list1_name']
-        list2_name = item['list2_name']
-        list1 = item['list1']
-        list2 = item['list2']
-        list2_set = set([tmp['url'] for tmp in list2])
-        doc_id = item['doc_id']
-        is_done = item['is_done']
+        img = item['image']
 
-        # list1中有一些项目，如果已经在list2中存在，则可以删除了
-        new_list1 = []
-        for list1_entry in list1:
-            url = list1_entry['url']
-            url2 = url if re.search(r'http://lvxingpai-img-store\.qiniudn\.com/(.+)', url) \
-                else 'http://lvxingpai-img-store.qiniudn.com/assets/images/%s' % hashlib.md5(url).hexdigest()
-            if url2 not in list2_set:
-                new_list1.append(list1_entry)
-
-        # 确保key字段
-        for l in [new_list1, list2]:
-            for entry in l:
-                if 'key' not in entry:
-                    match = re.search(r'http://lvxingpai-img-store\.qiniudn\.com/(.+)', entry['url'])
-                    if match:
-                        entry['key'] = match.group(1)
-
-        # 排序（按照favor_cnt和fSize排序）
-        def img_cmp(x, y):
-            fx = x['favor_cnt'] if 'favor_cnt' in x else 0
-            fy = y['favor_cnt'] if 'favor_cnt' in y else 0
-            if fx != fy:
-                return cmp(fx, fy)
-            else:
-                fsx = x['fSize'] if 'fSize' in x else 0
-                fsy = y['fSize'] if 'fSize' in y else 0
-                return cmp(fsx, fsy)
-
-        list2 = sorted(list2, cmp=img_cmp, reverse=True)
-
-        # 处理itemIds字段
         col_im = self.fetch_db_col('imagestore', 'Images', 'mongodb-general')
-        for image in list2:
-            key = image['key']
-            entry = col_im.find_one({'key': key})
-            id_set = set(entry['itemIds']) if 'itemIds' in entry else set([])
-            if doc_id not in id_set:
-                id_set.add(doc_id)
-                update_flag = True
-            else:
-                update_flag = False
-
-            if update_flag:
-                col_im.update({'_id': entry['_id']}, {'$set': {'itemIds': list(id_set)}})
-
-        col = self.fetch_db_col(db, col_name, 'mongodb-general')
-        # 如果已经尚未经历人工操作，则设置默认的images列表
-        if is_done:
-            ops = {}
+        col_im_c = self.fetch_db_col('imagestore', 'ImageCandidates', 'mongodb-general')
+        if 'itemIds' in img:
+            item_ids = img.pop('itemIds')
         else:
-            ops = {'$set': {list2_name: list2[:10]}}
-        if new_list1:
-            if '$set' not in ops:
-                ops['$set'] = {}
-            ops['$set'][list1_name] = new_list1
-        else:
-            spider.log('Unset %s for document: _id=%s' % (list1_name, doc_id))
-            ops['$unset'] = {list1_name: 1}
-        col.update({'_id': doc_id}, ops)
+            item_ids = None
+        ops = {'$set': img}
+        if item_ids:
+            ops['$addToSet'] = {'itemIds': {'$each': item_ids}}
+
+        col_im.update({'key': img['key']}, ops, upsert=True)
+        col_im_c.remove({'url_hash': img['url_hash']})
 
         return item
