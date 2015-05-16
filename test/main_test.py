148bc3c6d851ab7badec4d628de9fa321144e5dc
Zephyre <haizi.zh@gmail.com>
2015-05-17 02:11:38 +0800

    测试haversine函数

--- test/main_test.py
+++ test/main_test.py
@@ -3,18 +3,21 @@ __author__ = 'zephyre'
 import unittest
 
 
-def fun(x):
-    return x + 1
+class UtilsTest(unittest.TestCase):
+    def testHaversine(self):
+        from utils.utils import haversine
 
+        lon1, lat1 = 40, 50
+        lon2, lat2 = 30, 40
+        calc_dist = haversine(lon1, lat1, lon2, lat2)
+        actual_dist = 1358.4011247032208
+        self.assertAlmostEqual(calc_dist, actual_dist)
 
-class MainTest(unittest.TestCase):
-    def test(self):
-        self.assertEqual(fun(4), 5)
+        lat1 = -91
+        valid_param = True
+        try:
+            haversine(lon1, lat1, lon2, lat2)
+        except ValueError:
+            valid_param = False
 
-    def testAnother(self):
-        self.assertEqual(fun(8), 9)
-
-
-class FakeTest(unittest.TestCase):
-    def test(self):
-        self.assertEqual(fun(13), 14)
+        self.assertFalse(valid_param, "Failed in arguments validation")
