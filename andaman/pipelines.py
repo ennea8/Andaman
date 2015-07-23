f7087b07a579a44abd3d861d2e3494ca857efb5c
tar-zxvf <z_y_nan@sina.cn>
2015-07-23 18:51:38 +0800

    仅做存储代码用，其中pipiline为本地测试代码，middleware为修改海子的代码，celery为尝试代码

--- andaman/pipelines.py
+++ andaman/pipelines.py
@@ -12,12 +12,18 @@ from andaman.items.baidu import BaiduNoteItem
 class AndamanPipeline(object):
     spiders = []
     # 按照文档，进行基本的实现
+
     def __init__(self, mongo_host, mongo_port, mongo_user, mongo_passwd, mongo_dbname):
         self.mongo_host = mongo_host
         self.mongo_port = mongo_port
         self.mongo_user = mongo_user
         self.mongo_passwd = mongo_passwd
         self.mongo_dbname = mongo_dbname
+
+    # used for local test
+    # def __init__(self, mongo_uri, mongo_port):
+    #     self.mongo_uri = mongo_uri
+    #     self.mongo_port = mongo_port
 
     @classmethod
     def from_crawler(cls, crawler):
@@ -27,13 +33,20 @@ class AndamanPipeline(object):
             mongo_user=crawler.settings.get('MONGO_USER'),
             mongo_passwd=crawler.settings.get('MONGO_PASSWD'),
             mongo_dbname=crawler.settings.get('MONGO_DBNAME')
+
+            # used for local test
+            # mongo_uri=crawler.settings.get('MONGO_URI'), #'localhost'
+            # mongo_port=crawler.settings.get('MONGO_PORT') #27017
         )
 
     def open_spider(self, spider):
-        uri_one = 'mongodb://'+self.mongo_user+':'+self.mongo_passwd+'@'+self.mongo_host
-        uri_two = ':'+self.mongo_port+'/'+self.mongo_dbname
+        uri_one = 'mongodb://'+self.mongo_user[0]+':'+self.mongo_passwd[0]+'@'+self.mongo_host[0]
+        uri_two = ':'+self.mongo_port[0]+'/'+self.mongo_dbname[0]
         uri = uri_one + uri_two
         self.conn = pymongo.MongoClient(uri)
+
+        # used for local test
+        # self.conn = pymongo.MongoClient(self.mongo_uri, self.mongo_port)
 
     def close_spider(self, spider):
         self.conn.close()
@@ -47,7 +60,9 @@ class BaiduNotePipeline(AndamanPipeline):
         self.spiders = spider.name
 
         # 连接db,建Spider的db
-        db = self.conn[self.mongo_dbname]
+        db = self.conn[self.mongo_dbname[0]]
+        # used for local test
+        # db = self.conn[spider.name]
         one_note = item['one_note']
         query = {'note_id': one_note['note_id']}
         if isinstance(item, BaiduNoteItem):
