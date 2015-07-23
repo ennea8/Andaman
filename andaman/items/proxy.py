54896604673fccf34262552b1078b42edd9c25ce
Zephyre <haizi.zh@gmail.com>
2015-07-24 02:12:32 +0800

    ADD: 更新etcd中已有代理服务器的能力

--- andaman/items/proxy.py
+++ andaman/items/proxy.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import scrapy
 
 __author__ = 'zephyre'
@@ -10,3 +11,8 @@ class ProxyItem(scrapy.Item):
     desc = scrapy.Field()
     latency = scrapy.Field()
     verifiedTime = scrapy.Field()
+
+    # discard: 删除该代理服务器
+    # update_latency: 更新延迟
+    # default:
+    action = scrapy.Field()
