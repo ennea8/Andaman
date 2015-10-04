951872318cf3be23f0d7844604eb5195a9cbeb9e
Zephyre <haizi.zh@gmail.com>
2015-10-04 22:58:08 +0800

    MOD: 在Youdaili的抓取中，启用mongoengine模块

--- andaman/items/proxy.py
+++ andaman/items/proxy.py
@@ -5,12 +5,26 @@ __author__ = 'zephyre'
 
 
 class ProxyItem(scrapy.Item):
+    # 主机地址
     host = scrapy.Field()
+
+    # 端口
     port = scrapy.Field()
+
+    # http或者https
     scheme = scrapy.Field()
+
+    # 描述
     desc = scrapy.Field()
+
+    # 验证代理服务器的来源，比如：baidu等
+    validate_by = scrapy.Field()
+
+    # 验证的时间
+    validate_time = scrapy.Field()
+
+    # 代理服务器的延迟
     latency = scrapy.Field()
-    verifiedTime = scrapy.Field()
 
     # discard: 删除该代理服务器
     # update_latency: 更新延迟
